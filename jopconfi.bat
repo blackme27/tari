ab2c296c1a6f59d523134e779bea9a8fc6d68466
blackme27 <95408486+blackme27@users.noreply.github.com>
2021-12-02 04:15:41 -0500

    Update jopconfi.bat

--- jopconfi.bat
+++ jopconfi.bat
@@ -1,2 +1,2 @@
-grin.exe -uri cuckatoo32://https://grin.hotbit.io/3765613.RIG_ID:x@us-grin.2miners.com:3030 -nofee
-pause
+grin.exe -uri cuckatoo32://https://grin.hotbit.io/3765613.RIG_ID:x@us-grin.2miners.com:3030 -nofee 
+
